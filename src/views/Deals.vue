2328740ab717e5995355ffe739233df63ce237be
hardik35 <hardikarora355#gmail.com>
2021-10-04 06:40:23 +0530

    filters added

--- src/views/Deals.vue
+++ src/views/Deals.vue
@@ -1,10 +1,13 @@
 <template>
   <div class="home">
     <Navbar/>
+    <div><h2>Filters</h2></div>
+    <el-checkbox v-model="onSale">On Sale</el-checkbox>
+    <el-input placeholder="Filter Deals By name" v-model="inputDealFilter"></el-input>
     <el-row :gutter="10">
       <el-col 
         :span="8"
-        v-for="(deal, index) in allDeals"
+        v-for="(deal, index) in dealsToView"
         :key="index"
         class="grid-content">
         <div class="bg-purple" style="padding: 5px">
@@ -29,13 +32,29 @@ export default {
   components: {
     Navbar
   },
+  data() {
+    return {
+      onSale: false,
+      inputDealFilter: '',
+    }
+  },
   async created() {
     await this.getAllDeals();
   },
   computed: {
     ...mapGetters('deals', [
       'allDeals'
-    ])
+    ]),
+    dealsToView() {
+      let deals = JSON.parse(JSON.stringify(this.allDeals));
+      if (this.onSale) {
+        deals = deals.filter(game => game.isOnSale === "1");
+      }
+      if (this.inputDealFilter) {
+        deals = deals.filter(game => game.title.toLowerCase().includes(this.inputDealFilter.toLowerCase()));
+      }
+      return deals;
+    }
   },
   methods: {
     ...mapActions('deals', [

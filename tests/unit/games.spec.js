a26742e3e7d3c06a078965bc6247c874a8169a12
hardik35 <hardikarora355#gmail.com>
2021-10-04 06:59:10 +0530

    added test cases

--- tests/unit/games.spec.js
+++ tests/unit/games.spec.js
@@ -1,6 +1,33 @@
+import hashMapStores from '@/helperFunctions/hashMapStores'
 
 describe("Hasphmap stores data", () => {
   test('getting hashmap stores data', () => {
-    expect(mathOperations.sum(1, 2)).toBe(3);
+    expect(hashMapStores(
+      [
+        {
+          isActive: 1,
+          storeID: "1",
+          storeName: "Steam",
+          images: {
+            "banner": "/img/stores/banners/0.png",
+            "logo": "/img/stores/logos/0.png",
+            "icon": "/img/stores/icons/0.png"
+          }        
+        }
+      ]
+    )).toEqual(
+      {
+        "1": {
+          isActive: 1,
+          storeID: "1",
+          storeName: "Steam",
+          images: {
+            "banner": "/img/stores/banners/0.png",
+            "logo": "/img/stores/logos/0.png",
+            "icon": "/img/stores/icons/0.png"
+          }
+        }
+      }
+    );
   });
  })
